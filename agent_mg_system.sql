5c120ac2b9005ccee608fbcd275585bb3a714de0
amrcs92 <amrcs1992@gmail.com>
2019-04-08 02:45:38 +0200

    updated sql file

--- agent_mg_system.sql
+++ agent_mg_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 01, 2019 at 04:59 PM
+-- Generation Time: Apr 08, 2019 at 12:44 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.3.1
 
@@ -33,19 +33,18 @@ CREATE TABLE IF NOT EXISTS `reset_token` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `token` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `used_token` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `reset_token`
 --
 
-INSERT INTO `reset_token` (`id`, `user_id`, `token`) VALUES
-(3, 6, '41761422b2c3b24de5af9319a056c05bd955ee760fd5f3b5aa630fd0032f8935'),
-(4, 6, 'f78ac84bc84d0e3f005db86740371729faa914843ecf24b3dfd1db2d8bf27882'),
-(5, 6, '58a190e802300ade407e8a7ffb7837bf493830d582da3d905ee3935f2cef5dc0'),
-(6, 6, 'f40d7402259c01d64cdf035d088f8024fb9196916338eb7c48fd36fe3dea19da');
+INSERT INTO `reset_token` (`id`, `user_id`, `token`, `created_at`, `used_token`) VALUES
+(11, 8, 'df8b15ef57e52dbb59aa3d63713f84a4c8434125352df88bd29b2bd01b8b65df', '2019-04-06 15:08:37', 1);
 
 -- --------------------------------------------------------
 
@@ -73,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `company_name` (`company_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
@@ -81,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `agent_name`, `company_name`, `company_logo`, `phone`, `mobile1`, `mobile2`, `address`, `postal_code`, `country`, `state`) VALUES
 (4, 'shahin89', 'shahin@gmail.com', '$2y$10$TedMbej5b4IPgrxDenm8lOF2Bisb1saaKck.2kzCHPhlqjD4PYSO2', NULL, 'mohamed shahin', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(6, 'amrcs92', 'amrcs1992@gmail.com', '$2y$10$.tIrbDaq5nkpiYBC7.kI.e6i4ZCSp1Bo91He7Rcvw/xHUZyzItVUi', 'Amr Ashraf Serag Eldin', 'amr ashraf', '', '01092237499', NULL, NULL, 'Smouha', NULL, 'egypt', NULL);
+(8, 'amrcs92', 'amrcs1992@gmail.com', '$2y$10$aOwSDFFZ7D81IkeaFCgd2.nuUH9ULSZujikPrD3rTgzDG5qXxjOXC', NULL, 'Amr Ashraf', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -99,15 +98,52 @@ CREATE TABLE IF NOT EXISTS `user_history` (
   `last_login` timestamp NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `user_history`
 --
 
 INSERT INTO `user_history` (`id`, `user_id`, `ip_address`, `device_type`, `browser_details`, `last_login`) VALUES
-(15, 6, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-01 01:24:47'),
-(16, 6, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-01 01:43:52');
+(20, 4, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-02 23:52:20'),
+(31, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-06 15:12:41'),
+(32, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-06 15:16:38'),
+(33, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-06 15:21:14'),
+(34, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-06 15:27:05'),
+(35, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 13:28:32'),
+(36, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 13:37:33'),
+(37, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 19:18:32'),
+(38, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 19:19:42'),
+(39, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 19:24:26'),
+(40, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 19:26:00'),
+(41, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 19:49:34'),
+(42, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 19:50:25'),
+(43, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 19:53:46'),
+(44, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 20:01:37'),
+(45, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 20:38:16'),
+(46, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 20:48:50'),
+(47, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 21:46:54'),
+(48, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 21:49:43'),
+(49, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 21:49:58'),
+(50, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 21:53:50'),
+(51, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 22:31:27'),
+(52, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:24:02'),
+(53, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:28:37'),
+(54, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:29:04'),
+(55, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:38:19'),
+(56, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:39:16'),
+(57, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:39:39'),
+(58, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:44:22'),
+(59, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:49:20'),
+(60, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:49:37'),
+(61, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:50:04'),
+(62, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:51:30'),
+(63, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-07 23:52:00'),
+(64, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-08 00:04:26'),
+(65, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-08 00:05:03'),
+(66, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-08 00:05:34'),
+(67, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-08 00:09:11'),
+(68, 8, '::1', 'Windows 7', 'Chrome73.0.3683.86', '2019-04-08 00:10:14');
 
 --
 -- Constraints for dumped tables
